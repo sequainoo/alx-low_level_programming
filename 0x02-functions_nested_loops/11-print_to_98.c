d1d3ad2961b9f06b718527bfd69cd338eacb034d
sequainoo <wazatoyung@gmail.com>
2021-08-31 15:58:31 +0000

    Correct 11-print-to-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,12 +4,15 @@
 void print_to_98(int n)
 {
 	if (n==98)
+	{
 		printf("%d",n);
+		return;
+	}
 	
-	while (n < 98)
+	while (n <= 98)
 		printf("%d, ", n++);
 	
-	while (n > 98)
+	while (n >= 98)
 		printf("%d, ",n--);
 	
 	printf("\n");
