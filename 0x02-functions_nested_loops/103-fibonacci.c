d914993d72203208dd17191aff0e74423504e28e
sequainoo <wazatoyung@gmail.com>
2021-08-31 18:15:24 +0000

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fionacci sequence up to 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int fn;
+	long previous;
+	long current;
+	long next;
+	int i;
+	long sum;
+
+	previous = 1;
+	sum = 2;
+	current = 2;
+	fn = 4000000;
+	next = 0;
+	i = 3;
+
+	while (i <= fn)
+	{
+		next = previous + current;
+		previous = current;
+		current = next;
+		if (current % 2 == 0)
+			sum += current;
+		i++;
+	}
+	printf("%li\n", sum);
+
+	return (0);
+}
