675e5fa2313f23bf0ceb399bbd476c3b99831168
sequainoo <wazatoyung@gmail.com>
2021-08-31 15:49:19 +0000

    Add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+{
+	if (n==98)
+		printf("%d",n);
+	
+	while (n < 98)
+		printf("%d, ", n++);
+	
+	while (n > 98)
+		printf("%d, ",n--);
+	
+	printf("\n");
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -81,3 +81,9 @@ void times_table(void);
 
 int add(int a, int b);
 
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+
+void print_to_98(int n);
