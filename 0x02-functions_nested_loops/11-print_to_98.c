55e7ae5e5c7ce6ce6cad07ddefb7616883a1bd4c
sequainoo <wazatoyung@gmail.com>
2021-08-31 16:27:05 +0000

    Correct 11-print-to-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+
 void print_to_98(int n)
 {
 	while (n < 98)
 		printf("%d, ", n++);
-	
+
 	while (n > 98)
 		printf("%d, ", n--);
 	printf("%d\n", 98);
