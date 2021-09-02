fbff2a6b43b95ca77082b643214fd4e31adc6eaa
sequainoo <wazatoyung@gmail.com>
2021-09-02 17:54:17 +0000

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -15,4 +15,15 @@ void positive_or_negative(int i);
 
 int largest_number(int, int, int);
 
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
