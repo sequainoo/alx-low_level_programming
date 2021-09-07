1aa63f02a04750b47aa1f6a80978deb9ca60cb3d
sequainoo <wazatoyung@gmail.com>
2021-09-07 14:12:54 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  * _putchar - puts character to standard output
+ *   * @c: character to put to standard output
+ *    */
+
+int _putchar(char c);
+
+
+/**
+ *  * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ *   * @n: pointer to int n
+ *    */
+
+void reset_to_98(int *n);
+#endif
