4167dea43064198cc6ebbce3db19133432259955
sequainoo <wazatoyung@gmail.com>
2021-09-09 11:33:02 +0000

    Add all

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,14 @@ int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
+/**
+ * _strcat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
 #endif
