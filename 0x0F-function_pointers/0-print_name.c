ab9f1b285fefbe045377cc4a3fdcabd8c459eee5
sequainoo <wazatoyung@gmail.com>
2021-09-29 13:58:17 +0000

    Done commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,13 @@
+#include <stdlib.h>
+
 /**
- * print_name - a function that prints a name
- * @name: name to print that will be given to f
- * @f: a function pointer that prints name:
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ *
  */
 
-void print_name (char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 		f(name);
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -9,3 +9,9 @@ C programming language:
 
 ### Description of what each file shows:
 0. a function that prints a name via a function pointer
+1. a function that executees a function, given as a parameter, on each element of an array
+2. a function that searches for an integer via a function pointer
+3. a program that performs simple operator calculations (consist of 4 files starting with "3-")
+
+### Authors
+Gideon Q.
