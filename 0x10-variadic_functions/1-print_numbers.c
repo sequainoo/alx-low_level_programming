7ef74d094440c8614d1bb00fff475e4bf8cd894d
sequainoo <wazatoyung@gmail.com>
2021-09-30 06:06:20 +0000

    Add 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		return ;
-	
-	if (n < 1)
-		return ;
 
 	for (i = 0; i < n; i++)
 	{
@@ -27,6 +24,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 	va_end(args);
-
+	
 	printf("\n");
 }
