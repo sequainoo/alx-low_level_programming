9d7547c71d95ddfeb4e7f76c79d8c7b0faa9dc7b
sequainoo <wazatoyung@gmail.com>
2021-10-19 14:38:25 +0000

    AFix betty 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,17 @@ int create_file(const char *filename, char *text_content)
 	/* if not exists create with rw and open in write and trunc mode */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	/* if text content is empty dont write anything */
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 
 	/* find number of bytes to write */
 	len = 0;
@@ -33,7 +39,10 @@ int create_file(const char *filename, char *text_content)
 	/* write to file */
 	b_written = write(fd, text_content, len);
 	if (b_written == -1)
+	{
+		close(fd);
 		return (-1);
-
+	}
+	close(fd);
 	return (1);
 }
