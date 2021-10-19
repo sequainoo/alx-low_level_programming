5010431ba4a5ad2108a0ce85447c769fae1770f6
sequainoo <wazatoyung@gmail.com>
2021-10-19 14:41:46 +0000

    Refactored 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,16 @@ int create_file(const char *filename, char *text_content)
 	char *pos;
 	int b_written;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	/* if not exists create with rw and open in write and trunc mode */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		close(fd);
 		return (-1);
-	}
 
 	/* if text content is empty dont write anything */
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		close(fd);
 		return (1);
@@ -38,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 
 	/* write to file */
 	b_written = write(fd, text_content, len);
-	if (b_written == -1)
+	if (b_written == -1 || b_written != len)
 	{
 		close(fd);
 		return (-1);
