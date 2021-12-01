a520dd45930a49a050b5106ae4931f3570b88e24
sequainoo <wazatoyung@gmail.com>
2021-12-01 18:18:01 +0000

    Update fix pointer to head when insertion is at idx 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	dlistint_t *head = *h;
+	dlistint_t *head = *h, *prev = NULL, *next = NULL;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
@@ -18,18 +18,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (head != NULL)
 	{
+		prev = head->prev;
+		next = head;
 		if (count == idx)
 		{
-			new_node->next = head;
-			new_node->prev = head->prev;
-			if (head->prev != NULL)
-				head->prev->next = new_node;
-			head->prev = new_node;
-			*h = new_node;
+			new_node->next = next;
+			new_node->prev = prev;
+			if (prev != NULL)
+			{
+				prev->next = new_node;
+			}
+			next->prev = new_node;
+			if (idx == 0)
+			{
+				*h = new_node;
+			}
 			return (new_node);
 		}
 		count += 1;
 		head = head->next;
 	}
+	free(new_node);
 	return (NULL);
 }
