6d9fc4b09d6fb6aa6fcf065dcd7dd953e107939c
sequainoo <wazatoyung@gmail.com>
2021-12-17 14:07:34 +0000

    Update 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,61 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table
+ * @key: key
+ * @value: value
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node, *head, *elem;
+	unsigned long int index;
+	char *k;
+
+	if (key == NULL || strlen(key) == 0)
+		return (0);
+	k = _strcpy(key);
+	index = hash_djb2((const unsigned char *)k) % ht->size;
+	head = ht->array[index];
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = k;
+	node->value = _strcpy(value);
+
+	if (head == NULL)
+	{
+		node->next = NULL;
+		ht->array[index] = node;
+		return (1);
+	}
+	else
+	{
+		elem = head;
+		while (elem != NULL)
+		{
+			if (elem->key == k)
+			{
+				elem->value = strcat(elem->value, value);
+				free(node);
+				return (1);
+			}
+			elem = elem->next;
+		}
+		node->next = head;
+		ht->array[index] = node;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * _strcpy - copies a string
+ * @str: str to copy
+ * Return: char*
+ */
 char *_strcpy(const char *str)
 {
 	char *key;
@@ -18,54 +74,4 @@ char *_strcpy(const char *str)
 	key[i] = '\0';
 
 	return (key);
-}
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *node, *head, *elem;
-	unsigned long int index;
-	char *k;
-
-	if (key == NULL || strlen(key) == 0)
-		return (0);
-	k = _strcpy(key);
-
-	index = hash_djb2((const unsigned char *)k) % ht->size;
-	/* get the index of the key */
-	head = ht->array[index];
-
-	if (head == NULL)
-	{
-		node = malloc(sizeof(hash_node_t));
-		if (node == NULL)
-			return (0);
-
-		node->key = k;
-		node->value = _strcpy(value);
-		node->next = NULL;
-	}
-	else
-	{
-		/* if key already exists update value */
-		elem = head;
-		while (elem != NULL)
-		{
-			if (elem->key == k)
-			{
-				elem->value = strcat(elem->value, value);
-				return (1);
-			}
-			elem = elem->next;
-		}
-		/* if key doesnt exist already create node make it head */
-		node = malloc(sizeof(hash_node_t));
-		if (node == NULL)
-			return (0);
-
-		node->key = k;
-		node->value = _strcpy(value);
-		node->next = head;
-		return (1);
-	}
-	return (0);
 }
