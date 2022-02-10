ed90e90213aca57af3db9bdd337f9adfde58a212
sequainoo <wazatoyung@gmail.com>
2022-02-10 11:05:08 +0000

    Add fix for island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,16 +15,16 @@ def get_cell_perimeter(grid, row, col):
     '''
     perimeter = 0
 
-    if col == 0 or  grid[row][col-1]:
+    if grid[row][col-1] == 0:
         perimeter += 1
 
-    if row == 0 or grid[row - 1][col] == 0:
+    if grid[row - 1][col] == 0:
         perimeter += 1
 
-    if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+    if grid[row][col + 1] == 0:
         perimeter += 1
 
-    if row == len(grid) - 1 or grid[row + 1][col] == 0:
+    if grid[row + 1][col] == 0:
         perimeter += 1
 
     return perimeter
@@ -33,41 +33,49 @@ def get_cell_perimeter(grid, row, col):
 def get_row_perimeter(grid: list, row: int) -> int:
     '''It gets the perimeter of the landof the row.'''
     perimeter = 0
-    col = 0
+    col = 1
 
+    # from the start to end of the row
     while col < len(grid[row]):
-        if grid[row][col] == 1:
-            perimeter += get_cell_perimeter(grid, row, col)
+        # skip all water to a land
+        while grid[row][col] == 0 and col < len(grid[row]) - 1:
+            col += 1
+        if col == len(grid[row]) - 1:
+            break
+
+        # at this point a land was found and we get its perimeter
+        tmp_perimeter = get_cell_perimeter(grid, row, col)
+
+        # if perimeter is 4 then we've found an island
+        # if there is another island already ignore this lonely island
+        # otherwise return this lonely island
+        if tmp_perimeter == 4:
+            if perimeter < 1:
+                return tmp_perimeter
+        else:
+            perimeter += tmp_perimeter
         col += 1
+
     return perimeter
 
 
 def island_perimeter(grid: list) -> int:
     '''Find perimeter of the island given the grid.
+
+    Cell value 0 is water and value 1 is land.
+    A land's border makes a perimeter if:
+    1. It comes between water
+    2. Its a top border and the land is a boundary land at the top
+    3. Its left border or right border at the begining of the island or
+    end respectively.
+    4. Its a bottom boundary at the last row of the grid.
     '''
     perimeter = 0
-    row = 0
+    row = 1
 
     # for each row in the grid get the part of the land cells
     # that make a perimeter
-    while row < len(grid):
+    while row < len(grid) - 1:
         perimeter += get_row_perimeter(grid, row)
         row += 1
     return perimeter
-
-
-# def island_perimeter(grid):
-#     """ Calculate perimeter of grid where "1" is found"""
-#     p = 0
-#     for row in range(len(grid)):
-#         for col in range(len(grid[0])):
-#             if grid[row][col] == 1:
-#                 if row == 0 or grid[row - 1][col] == 0:
-#                     p += 1  # top
-#                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-#                     p += 1  # bottom
-#                 if col == 0 or grid[row][col - 1] == 0:
-#                     p += 1  # left
-#                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-#                     p += 1  # right
-#     return p
